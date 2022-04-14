81421f07460e098cce9d2484498564e703637686
Mathias Goncalves <goncalves.mathias@gmail.com>
2022-04-14 11:51:25 -0400

    TST: Leverage pre_mask on final_boldref_wf

--- fmriprep/workflows/bold/base.py
+++ fmriprep/workflows/bold/base.py
@@ -562,6 +562,7 @@ Non-gridded (surface) resamplings were performed using `mri_vol2surf`
         name="final_boldref_wf",
         omp_nthreads=omp_nthreads,
         multiecho=multiecho,
+        pre_mask=True,
     )
     final_boldref_wf.__desc__ = None  # Unset description to avoid second appearance
 
@@ -1181,6 +1182,7 @@ Non-gridded (surface) resamplings were performed using `mri_vol2surf`
         # remaining workflow connections
         (join_sdc_echos, final_boldref_wf, [
             ("corrected", "inputnode.bold_file"),
+            (("corrected_mask", pop_file), "inputnode.bold_mask"),
         ]),
         (join_sdc_echos, bold_t2s_wf, [
             (("corrected_mask", pop_file), "inputnode.bold_mask"),
